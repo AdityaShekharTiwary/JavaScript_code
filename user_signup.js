1d2ac19956d64a1a1a7fefe4a26d8069ed7f2b7e
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-09-22 15:30:57 +0530

    User Signup

---
+++ user_signup.js
@@ -0,0 +1,23 @@
+const uid="abc123";
+var fullName = "aditya tiwary";
+var email= "kyakarogejaanke@gmail.com"
+var password = "123456";
+var confirmPassword = "123456";
+var courseCount = 0;
+var isLoggedInFromGoogle = false;
+
+//fullName=prompt("Enter your name");
+
+console.log("Full name is: ",fullName);
+console.log(email);
+console.log(uid);
+
+console.log(`
+    with Unique ID:  ${uid}
+    useris:  &{fullName}
+    and his email is : ${email}
+    and uses the password: ${password}
+
+
+
+`)

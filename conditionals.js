0bdbf37a8e1cb2711e43c3e1877b6e14f8cfefb5
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-09-23 10:41:38 +0530

    Conditionals

---
+++ conditionals.js
@@ -0,0 +1,17 @@
+var temprature;
+
+// ToDo: Go to Google and get the data
+temprature = 15;
+if(temprature < 20)
+{
+    console.log("Its cold outside");
+}
+
+var email = true;
+var facebook = false;
+var google = false;
+
+if(email || facebook || google)
+{
+    console.log("Login Successful");
+}

de53a2c23f73f4ea4a3d746b36d716a11638d9e9
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-09-26 16:28:25 +0530

    Array

---
+++ array.js
@@ -0,0 +1,19 @@
+var countries = ["India","USA","England"];
+
+var states = new Array ("Bihar","UP","Jharkhand");
+
+console.log(countries[1]);
+console.log(states.length);
+states[0] = "Punjab";
+
+var user = ["Aditya","aditya@dev.in","3","34","True"];
+console.log(user);
+
+user.pop();
+console.log(user);
+
+user.unshift("NEW VALUE");
+console.log(user);
+
+user.shift();
+console.log(user);

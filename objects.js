d344ce56606ab1828288440b60f3ff8d15692b84
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-09-29 16:28:13 +0530

    Objects

---
+++ objects.js
@@ -0,0 +1,11 @@
+var user = {
+    firstName : "Aditya",
+    lastName : "Tiwary",
+    role : "Admin",
+    loginCount : 32,
+    facebookSignedIn : true,
+};
+
+console.log(user.firstName);
+console.log(user.lastName);
+console.log(user.loginCount);
